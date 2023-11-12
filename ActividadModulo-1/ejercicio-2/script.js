4d43c2dc4fe2836ccf002bd43b6695dce3d0b872
caladino-2023 <ladino.gaitan@gmail.com>
2023-11-12 18:51:52 -0500

    #modificado

--- ActividadModulo-1/ejercicio-2/script.js
+++ ActividadModulo-1/ejercicio-2/script.js
@@ -1,44 +1,29 @@
-"use sctrict"
-
-//Realizar el algoritmo que realice las 4 operaciones básicas para dos números de entrada.
-
 function calculate() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-    const operator = document.getElementById('operator').value;
-    let result = 0;
-  
-    switch (operator) {
-      case 'add':
-        result = num1 + num2;
-        break;
-      case 'subtract':
-        result = num1 - num2;
-        
-        result = num1 - num2;
-  
-        result =
-    break;
-      case 'multiply':
-        result = num1 * num2;
-        
-       
-  break;
-      case 'divide':
-        if (num2 !== 0) {
-          result = num1 / num2;
-        } 
-          result = num1 / num2;
-        }
-  
-          result = num1
-     {
-          result = "Cannot divide by zero";
-        }
-        break;
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  const operator = document.getElementById('operator').value;
+  let result = 0;
+
+  switch (operator) {
+    case 'add':
+      result = num1 + num2;
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      break;
+    case 'divide':
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        result = "Cannot divide by zero";
+      }
+      break;
     default:
-        result = "Invalid operator";
-    }
-  
-    document.getElementById('result').innerText = `Result: ${result}`;
-    }
+      result = "Invalid operator";
+  }
+
+  document.getElementById('result').innerText = `Result: ${result}`;
+}
